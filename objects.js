f6bb63a04be92a3c5019f11e315febe97ffc053d
Julia Miller <ihre.hexenhafte.hoheit@gmail.com>
2019-11-01 13:24:00 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,9 @@
-let playlist = {}
+let playlist = {Queen: 'We are the champions'}
+function updatePlaylist(playlist, name, song){
+  playlist.name = song;
+  return playlist;
+}
+function removeFromPlaylist(playlist, name){
+  delete playlist.name;
+  return playlist;
+}
